a3adac8a6749b02e3c52ece057c953c507ffba8a
EugeneHSS <90674960+EugeneHSS@users.noreply.github.com>
2023-10-17 01:28:17 +0800

    ECS merged with rendering
    Successfully merged to development branch.
    
    - Rendering system using layer 1 now. Need to consult Ruo Yan for this.
    
    - Rendering system now works with multiple entities however due to asset manager working well yet. You can only render spider model.
    
    - Engine will crash but it is due to vulkan mem leaks so that one I just need to clear the mem leaks.
    
    - You all can now start working on individual systems that need to see entity moving around. Aka physics, Ai.

--- GAM300Editor/entry.cpp
+++ GAM300Editor/entry.cpp
@@ -10,12 +10,10 @@
 
 #if _DEBUG
 #pragma comment( linker, "/subsystem:console" )
-int main(int argc, const char** argv) {
-<<<<<<<< HEAD:GAM300Editor/entry.cpp
+int main(int argc, const char** argv) 
+{
+
     (void)argc, (void)argv;
-========
-    
->>>>>>>> development:GAM300Engine/main.cpp
     return WinMain(GetModuleHandle(NULL), NULL, GetCommandLineA(), 0);
 }
 #else
