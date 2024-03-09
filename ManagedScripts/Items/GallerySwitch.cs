9b71eafb8eb8b88a34ada93ca925d8a746ae3e49
Celine <barnarnarpowar@hotmail.com>
2024-03-09 21:30:55 +0800

    Redo painting and note script, add gallery letter VO and subtitles

---
+++ ManagedScripts/Items/GallerySwitch.cs
@@ -0,0 +1,54 @@
+﻿/*!*************************************************************************
+****
+\file Item_Script.cs
+\author Celine Leong
+\par DP email: jiayiceline.leong@digipen.edu
+\par Course: csd3450
+\date 15-1-2024
+\brief  Gameplay script for player interaction with items
+****************************************************************************
+***/
+
+using ScriptAPI;
+using System;
+
+public class GallerySwitch : Script
+{
+    AudioComponent audioPlayer;
+
+    public static bool isActivated = false;
+    [SerializeField]
+    public GameObject? _InteractUI;
+
+    public override void Awake()
+    {
+        audioPlayer = gameObject.GetComponent<AudioComponent>();
+    }
+
+    public override void Start()
+    {
+
+    }
+
+    public override void Update()
+    {
+        if (gameObject.GetComponent<RigidBodyComponent>().IsRayHit())
+        {
+            Console.WriteLine("Gallery Switch");
+            _InteractUI.SetActive(true);
+            if (Input.GetKeyDown(Keycode.E))
+            {
+                isActivated = true;
+                if(GalleryLetter.isNotePicked)
+                {
+                    audioPlayer.play("pc_mighthaveopened");
+                }
+                else
+                {
+                    audioPlayer.play("pc_openedsomething");
+                }
+                GameplaySubtitles.counter = 30;
+            }
+        }
+    }
+}
